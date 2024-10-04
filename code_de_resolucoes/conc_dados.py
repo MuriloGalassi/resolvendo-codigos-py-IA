27ba5a2467c2b429bf4169251adbf42533512af8
Murilo Pedro Galassi <murilo.galassi@educacao.sp.gov.br>
2024-10-04 15:54:54 -0300

    add codigos

---
+++ code_de_resolucoes/conc_dados.py
@@ -0,0 +1,15 @@
+# Usando Operador 
+nome = input ("Digite o seu nome: ")
+
+sobrenome = input ("digite seu sobre nome: ")
+
+resultado = nome + " " + sobrenome
+
+print("seu nome é: ", resultado)
+
+#Usando Join
+#lista_dados = ["joao", "pedro"]
+
+#resultado = " ".join(lista_dados)
+
+#print(resultado)
---
+++ code_de_resolucoes/txt.py
@@ -0,0 +1,5 @@
+texto = input("Digite um texto: ")
+
+numero = int(input("Digite um número inteiro: "))
+
+print ((texto + ' ') * numero)
