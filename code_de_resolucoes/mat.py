5c71ee76f32f53ad8531a2565b3ba43e8fe019f2
Murilo Galassi <61015480+MuriloGalassi@users.noreply.github.com>
2024-10-05 00:08:39 +0000

    operacao mat

--- code_de_resolucoes/mat.py
+++ code_de_resolucoes/mat.py
@@ -1,1 +1,16 @@
+num1 = int(input)('Digite o primeiro numero inteiro: ')
 
+num2 = int(input)('Degite o segundo numero inteiro: ')
+
+operacao = input ('Digite a operação que deseja realizar (+, -, *, /): ')
+
+if operacao == '+':
+    print(num1 + num2)
+elif operacao == '-':
+    print(num1 - num2)
+elif operacao == '*':
+    print(num1 * num2)
+elif operacao == '/':
+    print(num1 / num2)  
+else:
+    print('Operação invalida') 
